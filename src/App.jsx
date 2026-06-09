02cde6f216f93c713b23b05672490ec6a0e25797
HatsuneM1kuFan <kylesbruington@gmail.com>
2026-06-08 20:46:08 -0500

    updated app.jsx around new character roster structure and multi team comp builds. Got characterSelector to show character icons on the left, will add team lineup next

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import { useState } from 'react'
 import CharacterSelector from './components/CharacterSelector'
+import characters from './data/Base_Kit.json'
 
 function App() {
   const [activePage, setActivePage] = useState('select')
-  const [team, setTeam] = useState([null, null, null, null])
+  const [roster, setRoster] = useState(Object.fromEntries(characters.map(char => [char.char_id, {character: char, lightcone: null, relics: null, planar: null}])))
+  const [teams, setTeams] = useState({"Team 1": [null, null, null, null]})
+  const [activeTeamId, setActiveTeamId] = useState("Team 1")
   const [selectedCharacter, setSelectedCharacter] = useState(null)
-
+  const activeTeam = teams[activeTeamId]
   const pages = [
     { id: 'select', label: 'Team' },
     { id: 'kit', label: 'Kit' },
     { id: 'build', label: 'Build' },
   ]
 
+  const setTeamSlot = (slotIndex, rosterEntry) => {
+    setTeams(prev => ({
+      ...prev,
+      [activeTeamId]: prev[activeTeamId].map((slot,i) => i === slotIndex ? rosterEntry : slot)}))} // prev[activeTeamId] is the team we are changing a character in, if i == slotIndex (the slot we want to change), replace with rosterEntry, else keep same slot state.
+    
+
   return (
     <div className="min-h-screen bg-gray-950 text-white p-6">
       <div className="flex items-center justify-between mb-6">
         <div>
           <h1 className="text-2xl font-bold text-white tracking-tight">HoukaiMoonTrain</h1>
-          <p className="text-gray-500 text-sm mt-1">HSR Build Simulator</p>
+          <p className="text-gray-500 text-sm mt-1">A Honkai: Star Rail Battle Simulator</p>
         </div>
         <nav className="flex gap-1">
           {pages.map(page => (
@@ -38,10 +47,9 @@ function App() {
 
       {activePage === 'select' && (
         <CharacterSelector
-          team={team}
-          setTeam={setTeam}
-          selectedCharacter={selectedCharacter}
-          setSelectedCharacter={setSelectedCharacter}
+          roster = {roster}
+          team = {activeTeam}
+          setTeamSlot = {setTeamSlot}
         />
       )}
       {activePage === 'kit' && (
